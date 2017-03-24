6a5e039002a5038effc4d281fd935da9f5806b8d
Michael Ulrich <michaeldulrich@gmail.com>
2017-03-24 07:52:08 -0400

    Index, Show, Edit, Update Actions
    Plus some templates related to those pages

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,20 +4,34 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new
+    # you could put a default value after .new, like Subject.new({:name => 'Default'})
   end
 
   def create
-
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to(subjects_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
-
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      redirect_to(subject_path(@subject))
+    else
+      render('edit')
+    end
   end
 
   def delete
@@ -26,4 +40,11 @@ class SubjectsController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+
 end
