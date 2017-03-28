6f6477c66755d5bb5ce0b191cb72ad0fccd15eb5
Michael Ulrich <michaeldulrich@gmail.com>
2017-03-28 09:05:56 -0400

    Added missing headers. belongs_to no longer optional

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ApplicationRecord
 
-  belongs_to :page, {:optional => true}
+  belongs_to :page #, {:optional => true}
   has_many :section_edits
   has_many :admin_users, :through => :section_edits
 
