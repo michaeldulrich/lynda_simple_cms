36cabbe65a0e175bf3ee7c13b0400f5761077db2
Michael Ulrich <michaeldulrich@gmail.com>
2017-04-04 07:10:17 -0400

    Data validation updates

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,5 +10,12 @@ class Page < ApplicationRecord
   scope :newest_first, lambda { order("created_at DESC") }
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"] ) }
 
-
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, :within => 3..255
+  # use presence_of with length_of to disallow spaces
+  validates_uniqueness_of :permalink
+  # for unique values by subject use ":scope => :subject_id"
+  
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -10,4 +10,13 @@ class Section < ApplicationRecord
   scope :newest_first, lambda { order("created_at DESC") }
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"] ) }
 
+
+  CONTENT_TYPES = ['text', 'HTML']
+
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_inclusion_of :content_type, :in => CONTENT_TYPES,
+    :message => "must be one of: #{CONTENT_TYPES.join(', ')}"
+  validates_presence_of :content
+
 end
